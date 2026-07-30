831cd572bf448bd15ce93d379780e744abcbd495
Haebsa <hamidebisoft@gmail.com>
2026-07-30 14:51:17 +0330

    Update config.js

--- config.js
+++ config.js
@@ -16,3 +16,32 @@ export const CONFIG = {
     timeout: 20000
 
 };
+module.exports = {
+
+  footer: `
+━━━━━━━━━━━━
+📢 @Khodroo_Akhbar
+`,
+
+  rubika: [
+    {
+      token: "...",
+      chatId: "@channel1"
+    }
+  ],
+
+  eitaa: [
+    {
+      token: "...",
+      chatId: "@channel2"
+    }
+  ],
+
+  bale: [
+    {
+      token: "...",
+      chatId: "@channel3"
+    }
+  ]
+
+};
