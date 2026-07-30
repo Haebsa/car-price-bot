c55a7dfd3b57cc59446a4810863ecce47907e713
Haebsa <hamidebisoft@gmail.com>
2026-07-30 14:38:17 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,18 @@
 
+export const CONFIG = {
+
+    sources: {
+
+        iranjib: true,
+
+        hamrahmechanic: true,
+
+        bama: true,
+
+        divar: false
+
+    },
+
+    timeout: 20000
+
+};
