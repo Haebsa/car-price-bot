cbd61595bfcc9edbcd1ced403c7fb1bce70a7405
Haebsa <hamidebisoft@gmail.com>
2026-07-30 14:51:57 +0330

    Update sendRubika.js

--- senders/sendRubika.js
+++ senders/sendRubika.js
@@ -1,1 +1,42 @@
+const axios = require("axios");
 
+// config.js را می‌خواند
+const { RUBIKA_BOTS } = require("./config");
+
+async function sendRubika(message) {
+
+    for (const bot of RUBIKA_BOTS) {
+
+        try {
+
+            const url =
+                `https://botapi.rubika.ir/v3/${bot.token}/sendMessage`;
+
+            const res = await axios.post(url, {
+                chat_id: bot.chatId,
+                text: message
+            });
+
+            console.log(
+                `✅ ارسال شد -> ${bot.chatId}`,
+                res.data
+            );
+
+        } catch (err) {
+
+            console.log(
+                `❌ خطا برای ${bot.chatId}`
+            );
+
+            if (err.response)
+                console.log(err.response.data);
+            else
+                console.log(err.message);
+
+        }
+
+    }
+
+}
+
+module.exports = { sendRubika };
