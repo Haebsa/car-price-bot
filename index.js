2bad0345bf46c4b14fc548281fe105efe2a0215a
Haebsa <hamidebisoft@gmail.com>
2026-07-30 10:18:03 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Car Price Bot Started...");
+const axios = require("axios");
+
+async function testIranJib() {
+    try {
+        const res = await axios.get("https://www.iranjib.ir/showgroup/45/");
+
+        console.log("Status:", res.status);
+        console.log("Length:", res.data.length);
+
+        console.log(res.data.substring(0, 500));
+
+    } catch (err) {
+        console.error(err.message);
+    }
+}
+
+testIranJib();
