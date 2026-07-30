36ed987b1bc5016b26b9681d0230f4a662f43ad5
Haebsa <hamidebisoft@gmail.com>
2026-07-30 14:58:04 +0330

    Update scheduler.js

--- scheduler.js
+++ scheduler.js
@@ -1,1 +1,39 @@
+const fs = require("fs");
 
+const FILE = "lastSend.json";
+
+function canSend(minutes) {
+
+    let data = {};
+
+    if (fs.existsSync(FILE)) {
+        data = JSON.parse(fs.readFileSync(FILE));
+    }
+
+    const now = Date.now();
+
+    if (!data.lastSend) {
+
+        data.lastSend = now;
+
+        fs.writeFileSync(FILE, JSON.stringify(data));
+
+        return true;
+    }
+
+    const diff = (now - data.lastSend) / 60000;
+
+    if (diff >= minutes) {
+
+        data.lastSend = now;
+
+        fs.writeFileSync(FILE, JSON.stringify(data));
+
+        return true;
+    }
+
+    return false;
+
+}
+
+module.exports = { canSend };
