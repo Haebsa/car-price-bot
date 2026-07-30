e7d2abd563882f3b13d04083021504de68622bfc
Haebsa <hamidebisoft@gmail.com>
2026-07-30 10:00:00 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
 //main.js
+import { getIranJibPrices } from "./sources/iranjib.js";
+
+async function main() {
+
+    console.log("شروع دریافت قیمت خودرو...");
+
+    const cars = await getIranJibPrices();
+
+    console.log(cars);
+
+}
+
+main();
