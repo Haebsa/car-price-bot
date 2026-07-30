3d69a3184e1c2684f9f8c69797c4a027376b2e5e
Haebsa <hamidebisoft@gmail.com>
2026-07-30 14:40:42 +0330

    Update formatter.js

--- formatter.js
+++ formatter.js
@@ -1,1 +1,38 @@
+function formatNumber(price) {
 
+    if (price == null) return "-";
+
+    return (price / 1000000000).toFixed(3);
+
+}
+
+function buildMessage(cars){
+
+    let text = "🚗 قیمت روز خودرو\n\n";
+
+    let currentBrand = "";
+
+    cars.forEach(car=>{
+
+        if(car.brand !== currentBrand){
+
+            currentBrand = car.brand;
+
+            text += "🏭 " + currentBrand + "\n\n";
+
+        }
+
+        const factory = car.factory == null ? car.factoryText : formatNumber(car.factory);
+        const market  = car.market == null ? car.marketText : formatNumber(car.market);
+
+        text += `${car.name}  🏭${factory}  💵${market}\n`;
+
+    });
+
+    return text;
+
+}
+
+module.exports = {
+    buildMessage
+};
