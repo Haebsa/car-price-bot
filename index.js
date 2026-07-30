df0ac36014b4abf44ddf85dacabf6b1637806d84
Haebsa <hamidebisoft@gmail.com>
2026-07-30 12:44:17 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-async function inspect() {
-    const { data } = await axios.get("https://www.iranjib.ir/showgroup/45/", {
-        headers: {
-            "User-Agent": "Mozilla/5.0"
+async function inspectIranJib() {
+
+    const { data } = await axios.get(
+        "https://www.iranjib.ir/showgroup/45/",
+        {
+            headers: {
+                "User-Agent": "Mozilla/5.0"
+            }
         }
-    });
+    );
 
     const $ = cheerio.load(data);
 
-    console.log("Title:", $("title").text());
-
     console.log("Tables:", $("table").length);
 
-    $(".items_table").each((i) => {
-        console.log("items_table", i);
-        console.log($.html($(".items_table").eq(i)).substring(0, 800));
-        console.log("================================");
+    $("table").each((i, table) => {
+
+        console.log("==============");
+        console.log("TABLE", i);
+
+        console.log(
+            $(table).attr("class")
+        );
+
     });
+
 }
 
-inspect();
+inspectIranJib();
